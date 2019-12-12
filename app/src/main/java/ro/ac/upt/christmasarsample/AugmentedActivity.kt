e765620f44974c239bb29c4a1ed65692b11893f1
Daniel Bejan <dannyb97danny@gmail.com>
2019-12-12 17:28:05 +0200

    Implementation

--- app/src/main/java/ro/ac/upt/christmasarsample/AugmentedActivity.kt
+++ app/src/main/java/ro/ac/upt/christmasarsample/AugmentedActivity.kt
@@ -1,16 +1,10 @@
 package ro.ac.upt.christmasarsample
 
-import android.app.Activity
-import android.app.ActivityManager
-import android.content.Context
 import android.net.Uri
-import android.os.Build
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.util.Log
-import android.widget.Toast
 import com.google.ar.core.Anchor
-import com.google.ar.core.HitResult
 import com.google.ar.sceneform.AnchorNode
 import com.google.ar.sceneform.rendering.ModelRenderable
 import com.google.ar.sceneform.rendering.Renderable
@@ -22,7 +16,7 @@ class AugmentedActivity : AppCompatActivity() {
 
     private lateinit var arFragment: ArFragment
 
-    private var renderable: Renderable? = null
+    private var renderable: ModelRenderable? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,30 +26,50 @@ class AugmentedActivity : AppCompatActivity() {
 
         arFragment = supportFragmentManager.findFragmentById(R.id.scf_central) as ArFragment
 
-        TODO("2. Invoke addRenderableToScene once a tap is executed over the AR plane")
+//        TODO("2. Invoke addRenderableToScene once a tap is executed over the AR plane")
 
 
+        arFragment.setOnTapArPlaneListener { hr, p, me ->
+
+            val anchor = hr.createAnchor()
+            addRenderableToScene(anchor = anchor, renderable = renderable!!)
+
+        }
     }
 
     private fun initRenderableModel() {
         val modelUri = Uri.parse("model.sfb")
 
-        TODO("1. Init model renderable variable")
-
+//        TODO("1. Init model renderable variable")
+        ModelRenderable.builder()
+            .setSource(this, modelUri)
+            .build()
+            .thenAccept { r -> renderable = r }
+            .exceptionally { throwable ->
+                Log.e(TAG, "Unable to load Renderable.", throwable)
+                null
+            }
 
     }
 
     private fun addRenderableToScene(anchor: Anchor, renderable: Renderable) {
-        TODO("3. Build an anchor node and set the AR scene to be its parent")
+//        TODO("3. Build an anchor node and set the AR scene to be its parent")
+
+        val node = AnchorNode(anchor)
+        node.setParent(arFragment.arSceneView.scene)
 
 
-        TODO("4. Build an transformable node and set the previously anchor node to be its parent")
+//        TODO("4. Build an transformable node and set the previously anchor node to be its parent")
+        val transformableNode = TransformableNode(arFragment.transformationSystem)
 
 
-        TODO("5. Assign node's renderable property to previously loaded renderable")
-
-
+//        TODO("5. Assign node's renderable property to previously loaded renderable")
+        transformableNode.renderable = renderable
+        transformableNode.setParent(node)
+        transformableNode.select()
     }
+
+
 
     companion object {
         private val TAG = AugmentedActivity::class.java.simpleName
